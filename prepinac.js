4eea46653766740442e77dd1f6e29866935ec745
antoninbrejcha <brejcha.an.2023@skola.ssps.cz>
2025-06-18 14:05:49 +0200

    Implement multiple code changes to enhance functionality and improve performance

--- prepinac.js
+++ prepinac.js
@@ -1,14 +1,20 @@
 let button = document.getElementById("button");
 let display = document.getElementById("display");
 let background = document.getElementById("background");
+let glowingLamp = document.getElementById("glowing-lamp");
+let lamp = document.getElementById("lamp");
 let dark = false;
 
 button.addEventListener("click", () => {
   if (dark) {
+    lamp.style.display = "none";
+    glowingLamp.style.display = "block";
     background.style.backgroundColor = "white";
     display.innerHTML = "Vypnout světlo";
     dark = false;
   } else {
+    glowingLamp.style.display = "none";
+    lamp.style.display = "block";
     background.style.backgroundColor = "black";
     display.innerHTML = "Zapnout světlo";
     dark = true;
